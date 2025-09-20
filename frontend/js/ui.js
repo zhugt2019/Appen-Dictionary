f0a6acaa74f5505dc04fb19e8a1f3b5f7e786107
Max Zhu <167700202+zhugt2019@users.noreply.github.com>
2025-09-20 13:57:56 +0200

    Small fix to display the persian language correctly.

--- frontend/js/ui.js
+++ frontend/js/ui.js
@@ -277,22 +277,22 @@ const reportLabels = {
         synonyms: 'Từ đồng nghĩa',
         antonyms: 'Từ trái nghĩa'
     },
-    'fa': {
-    definition: 'تعریف',
-    partOfSpeech: 'نوع کلمه',
-    ipa: 'الفبای آوانگاری بین‌المللی (IPA)',
-    inflections: 'صرف فعل/اسم',
-    comparison: 'حالت تفضیلی',
-    exampleSentences: 'جملات نمونه',
-    synonyms: 'مترادف‌ها',
-    antonyms: 'متضادها'
+    'fa': { // 
+        definition: 'تعریف',
+        partOfSpeech: 'نوع کلمه',
+        ipa: 'الفبای آوانگاری بین‌المللی (IPA)',
+        inflections: 'صرف فعل/اسم',
+        comparison: 'حالت تفضیلی',
+        exampleSentences: 'جملات نمونه',
+        synonyms: 'مترادف‌ها',
+        antonyms: 'متضادها'
     },
     // Fallback or English if target language not found
     'default': {
         definition: 'Definition',
         partOfSpeech: 'Part of Speech',
         inflections: 'Inflections',
-        comparison: 'Comparison', // <--- ADDED
+        comparison: 'Comparison',
         exampleSentences: 'Example Sentences',
         synonyms: 'Synonyms',
         antonyms: 'Antonyms'
